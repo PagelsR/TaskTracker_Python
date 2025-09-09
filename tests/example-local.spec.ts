0cae6e2afe75c72a9eccdde37c8e57592ede2c0f
Randy Pagels <pageslr@live.com>
2025-09-08 17:41:17 -0700

    Update Playwright test to check for the correct page title 'VSLive San Diego!'

--- tests/example-local.spec.ts
+++ tests/example-local.spec.ts
@@ -4,7 +4,7 @@ test('has title', async ({ page }) => {
   await page.goto('http://127.0.0.1:5000/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/.NET Users Group/);
+  await expect(page).toHaveTitle(/VSLive San Diego!/);
 });
 
 test('get started link', async ({ page }) => {
